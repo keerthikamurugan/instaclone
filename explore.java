c622e9dfd2b7579036f6069dfca7ea215f9af70a
keerthikamurugan <113917236+keerthikamurugan@users.noreply.github.com>
2023-08-21 22:51:42 +0530

    Create explore.java

---
+++ explore.java
@@ -0,0 +1,16 @@
+package com.example.newinsta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class explore extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.explore);
+    }
+}
